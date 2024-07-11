000edf961b90bac0876722d1d07706c4fab5604b
salmanfaris22 <salman567633800@gmail.com>
2024-07-11 12:07:35 +0530

    first commit

---
+++ react/js.js
@@ -0,0 +1,23 @@
+
+// function abc(cb){
+//     me= false
+//    return new Promise((res,rej)=>{
+//       if(me){
+//         res("hy i'm hear")
+//       }else{
+//         rej("i'm not exits")
+//       }
+//    })
+   
+  
+// }
+
+// abc().then((res)=>console.log(res))
+//      .catch((err)=>console.log(err))
+
+
+let a=10
+function abc(){
+
+}
+
